d08780c6a554f24f3d0723a7d0f01392d26d6071
khoatrinh109 <kroztrinh69@gmail.com>
2023-08-06 16:02:00 +0700

    QLCB

---
+++ QLCB/DanhSachNhanVien.h
@@ -0,0 +1,13 @@
+#pragma once
+#pragma once
+#include "Header.h"
+#include "LinkedList.h"
+
+template <class NhanVien>
+class DanhSachNhanVien :public LinkedList<NhanVien> {
+public:
+	void xuLyDocFile();
+	NhanVien* timKiemNhanVien(string username, string password);
+
+};
+
